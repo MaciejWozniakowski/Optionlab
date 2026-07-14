2b552a382e9c6770b85e8fa28e7c975d9442ad25
Maciej Wozniakowski <maciejwozniakowski02@gmail.com>
2026-07-14 19:59:04 +0200

    fixed a bug

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,6 @@
 #include "data_parser.hpp" 
 
 int main() {
-    const std::string api_token = "d9b66u1r01qmk4gkdjs0d9b66u1r01qmk4gkdjsg";
     optionlab::DataGatherer feed(sdt::getenv("api_token"));
     feed.subscribe("NVDA");
     feed.on_tick([](const optionlab::Trade& t));
